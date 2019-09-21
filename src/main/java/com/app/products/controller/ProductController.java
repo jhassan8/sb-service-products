388be3ac18098debf43a1205ae5ad04f5dadfb32
Jocs <jhassan8@outlook.com>
2019-09-21 14:54:38 -0300

    Error tolerance and latency with Hystrix

--- src/main/java/com/app/products/controller/ProductController.java
+++ src/main/java/com/app/products/controller/ProductController.java
@@ -5,7 +5,6 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,31 +16,28 @@ import com.app.products.models.service.IProductService;
 public class ProductController {
 
 	private IProductService iProductService;
-	private Environment envirenment;
 	
 	@Value("${server.port}")
 	private Integer port;
 	
 	@Autowired
-	public ProductController(IProductService iProductService, Environment envirenment) {
+	public ProductController(IProductService iProductService) {
 		this.iProductService = iProductService;
-		this.envirenment = envirenment;
 	}
 	
 	@GetMapping("/products")
 	public List<Product> products() {
 		return this.iProductService.findAll().stream().map(p -> {
-			//p.setPort(Integer.parseInt(envirenment.getProperty("local.server.port")));
 			p.setPort(port);
 			return p;
 		}).collect(Collectors.toList());
 	}
 	
 	@GetMapping("/product/{id}")
-	public Product product(@PathVariable Long id) {
+	public Product product(@PathVariable Long id) throws Exception {
 		Product product = this.iProductService.findById(id);
-		//product.setPort(Integer.parseInt(envirenment.getProperty("local.server.port")));
 		product.setPort(port);
+		
 		return product;
 	}
 	
