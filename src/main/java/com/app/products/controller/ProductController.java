b1cef25d45c34293e7edb88085b967d35e5e2430
Jocs <jhassan8@outlook.com>
2019-09-22 12:41:44 -0300

    CRUD services

--- src/main/java/com/app/products/controller/ProductController.java
+++ src/main/java/com/app/products/controller/ProductController.java
@@ -5,8 +5,14 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.app.products.models.entity.Product;
@@ -47,4 +53,24 @@ public class ProductController {
 		return product;
 	}
 	
+	@PostMapping("new")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Product create(@RequestBody Product product) {
+		return this.iProductService.save(product);
+	}
+	
+	@PutMapping("update/{id}")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Product update(@RequestBody Product product, @PathVariable Long id) {
+		Product p = iProductService.findById(id);
+		p.setName(product.getName());
+		p.setPrice(product.getPrice());
+		return this.iProductService.save(p);
+	}
+	
+	@DeleteMapping("delete/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void delte(@PathVariable Long id) {
+		this.iProductService.deleteById(id);
+	}
 }
--- src/main/java/com/app/products/models/service/IProductService.java
+++ src/main/java/com/app/products/models/service/IProductService.java
@@ -8,5 +8,7 @@ public interface IProductService {
 
 	public List<Product> findAll();
 	public Product findById(Long id);
+	public Product save(Product product);
+	public void deleteById(Long id);
 	
 }
